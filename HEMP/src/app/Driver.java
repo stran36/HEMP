29ea56ff1168dec4f05066ce936c870fb38f0d25
TheAndyEcheverria <aecheve9@calstatela.edu>
2018-04-05 21:11:17 -0700

    Pushing from eclipse

---
+++ HEMP/src/app/Driver.java
@@ -0,0 +1,18 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Driver extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
