c97df3e9842f38946a5bb824627e77e002d70f85
TheAndyEcheverria <aecheve9@calstatela.edu>
2018-04-06 18:38:57 -0700

    Updated Driver and pushing styles.css

--- HEMP/src/app/Driver.java
+++ HEMP/src/app/Driver.java
@@ -1,14 +1,20 @@
 package app;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import visuals.GUI;
 
 public class Driver extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+		GUI gui = new GUI();
+		Scene scene = new Scene(gui);
+		scene.getStylesheets().add("app/styles.css");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
